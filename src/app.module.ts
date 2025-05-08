f57bd6da26d3d84d99dcca02e2bd9bd41d3038f7
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2025-05-08 13:17:14 +0200

    pre-production changes

---
+++ src/app.module.ts
@@ -0,0 +1,43 @@
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import cookieSession from 'cookie-session';
+// import { dataSourceOptions } from 'typeorm.config';
+import { dbConfig } from '../ormconfig';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+    // TypeOrmModule.forRoot(dataSourceOptions),
+    TypeOrmModule.forRoot(dbConfig),
+    UsersModule,
+    ReportsModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    { provide: APP_PIPE, useValue: new ValidationPipe({ whitelist: true }) },
+  ],
+  exports: [TypeOrmModule],
+})
+export class AppModule {
+  constructor(private configService: ConfigService) {}
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: [this.configService.get('COOKIE_KEY') || 'randomCookieString'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
